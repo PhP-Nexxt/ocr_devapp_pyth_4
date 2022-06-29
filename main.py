8f7139ed97e9a72f028b56fa3980ee475c28aed5
philippe_pras <ph.pras@gmail.com>
2022-06-29 16:11:18 +0200

    Update avec test flake8

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 
+
 from chess.controllers import Application
+
 
 def main(args):
     if len(args) < 2:
         print(f"Usage: {args[0]} <database path>")
         raise SystemExit(-1)
-    
     # Unpack the args list to extract the database path from args[1]
     database_path, *_ = args[1:]
     app = Application(database_path)
